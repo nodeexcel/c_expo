158966498e81a1a655feea91a3cd5571f2676c9d
Git-Abhay7 <abhayrajawat102@gmail.com>
2020-08-31 19:50:22 +0530

    fetching by imageKey

--- config/database.js
+++ config/database.js
@@ -8,8 +8,8 @@ module.exports = ({ env }) => ({
         host: env('DATABASE_HOST', '127.0.0.1'),
         port: env.int('DATABASE_PORT', 3306),
         database: env('DATABASE_NAME', 'c_expo'),
-        username: env('DATABASE_USERNAME', 'dbuser'),
-        password: env('DATABASE_PASSWORD', 'dbuser@123'),
+        username: env('DATABASE_USERNAME', 'root'),
+        password: env('DATABASE_PASSWORD', 'MySQL#root123'),
         ssl: env.bool('DATABASE_SSL', false),
       },
       options: {}
